f73015b52e3214cb40a3e85714d660531e35c73a
TonyStark9 <38134646+TonyStark9@users.noreply.github.com>
2022-05-26 15:51:52 +0200

    added rtt-ping plot and boxplot

--- analysis/data_analysis.py
+++ analysis/data_analysis.py
@@ -3,10 +3,17 @@ import pandas as pd
 import numpy as np
 from matplotlib import pyplot as plt
 from analysis_utils import *
+import os
 
 def read_file(filename):
     with open(filename) as f:
         return f.read()
+
+def get_description(dump):
+    lines = dump.split("\n")
+    l = [l[1:] for l in lines if (l and (l[0] == '#'))]
+    return '\n'.join(l)
+
 
 def plot_RTT_per_ping(filename):
     dump = read_file(filename)
@@ -16,4 +23,31 @@ def plot_RTT_per_ping(filename):
     ax.set_xlabel("ping number")
     ax.set_ylabel("RTT (ms)")
     ax.set_title("RTT for each ping req".title())
+    plt.figtext(1.33, 0.65, get_description(dump), wrap=True, horizontalalignment='center', fontsize=12)
     s.plot(ax=ax)
+
+def find_timeout_indexes(s):
+    timeout_index_list = []
+    for i in range(len(s)):
+        if s[i] == 4000:
+            timeout_index_list.append(i)
+    return timeout_index_list
+
+def plot_boxplot(path):
+    my_dict = {}
+    i = 0
+    for filename in os.listdir(path):
+        i+= 1
+        s = pd.Series(dump_to_rtt_list(read_file(path + filename)))
+        first_in_first = find_timeout_indexes(s)[0]
+        one_last_thing = find_timeout_indexes(s)[-1]
+        attack_rtts = s[first_in_first:one_last_thing+1]
+        if i == 1:
+            no_attack_rtts = s[-(one_last_thing+1):first_in_first]
+            my_dict["No attack"] = no_attack_rtts
+        my_dict["Attack_"+str(i)] = attack_rtts
+
+    fig, ax = plt.subplots()
+    ax.set_title('Basic Plot')
+    ax.boxplot(my_dict.values(), showmeans=True)
+    ax.set_xticklabels(my_dict.keys())
